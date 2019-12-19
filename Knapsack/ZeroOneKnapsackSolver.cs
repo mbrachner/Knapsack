2a5db1a4ac8653eb98e9181ea805b39444abdb74
Markus Brachner <Markus.Brachner@sintef.no>
2019-12-19 15:27:31 +0100

    Initiial version

--- Knapsack/ZeroOneKnapsackSolver.cs
+++ Knapsack/ZeroOneKnapsackSolver.cs
@@ -10,19 +10,76 @@ namespace Knapsack
 	{
 		private readonly uint[] w;
 		private readonly double[] v;
-		private Dictionary<uint,uint> value = new Dictionary<uint, uint>();
+		private readonly uint capacity;
+		private Dictionary<(uint,uint), double> valueCache 
+			= new Dictionary<(uint numItems, uint capacity), double>();
 
 		public ZeroOneKnapsackSolver(uint[] w, double[] v, uint capacity)
 		{
 			this.w = w;
 			this.v = v;
+			this.capacity = capacity;
 		}
 
 		public int Objective { get; private set; }
 
-		public void Solve()
+		private double value(uint numItems, uint capacity)
 		{
-			throw new NotImplementedException();
+			if (numItems == 0 || capacity == 0)
+			{
+				return 0;
+			}
+
+			if (valueCache.TryGetValue((numItems, capacity), out var value))
+				return value;
+			else
+			{
+				var v = Solve(numItems, capacity);
+				valueCache[(numItems, capacity)] = v;
+				return v;
+			}
+		}
+
+		public double Solve(uint numItems, uint capacity)
+		{
+			if (numItems==0 || capacity == 0)
+			{
+				return 0;
+			}
+
+			if (w[numItems-1] > capacity)
+			{
+				valueCache[(numItems, capacity)] = value(numItems - 1, capacity);
+			}
+			else
+			{
+				valueCache[(numItems, capacity)] = Math.Max(
+					value(numItems - 1, capacity),
+					value(numItems - 1, capacity - w[numItems-1]) + v[numItems-1]
+				);
+			}
+
+			return valueCache[(numItems, capacity)];
+		}
+
+		public List<bool> GetSelectedItems(uint numItems, uint capacity)
+		{
+			var selectionList = new List<bool>();
+			var currentCapacity = capacity;
+			for (var currentItem = numItems; currentItem > 0; currentItem--)
+			{
+				bool isSelected;
+				if (value(currentItem-1, currentCapacity)==value(currentItem, currentCapacity))
+				{
+					isSelected = false;
+				} else
+				{
+					isSelected = true;
+					currentCapacity -= this.w[currentItem - 1];
+				}
+				selectionList.Insert(0, isSelected);
+			}
+			return selectionList;
 		}
 	}
 }
--- KnapsackTests/UnitTest1.cs
+++ KnapsackTests/UnitTest1.cs
@@ -10,11 +10,21 @@ namespace KnapsackTests
 		[TestMethod]
 		public void TestMethod1()
 		{
-			var w = new double[] { 23, 26, 20, 18, 32, 27, 29, 26, 30, 27 };
+			var w = new uint[] { 23, 26, 20, 18, 32, 27, 29, 26, 30, 27 };
 			var v = new double[] { 505, 352, 458, 220, 354, 414, 498, 545, 473, 543};
-			var solver = new ZeroOneKnapsackSolver(w, v);
-			solver.Solve();
-			Assert.AreEqual(solver.Objective, 1270);
+			var solver = new ZeroOneKnapsackSolver(w, v, 67);
+			var res = solver.Solve(10, 67);
+			Assert.AreEqual(res, 1270);
+		}
+
+		[TestMethod]
+		public void TestMethod2()
+		{
+			var w = new uint[] { 23, 26, 20, 18, 32, 27, 29, 26, 30, 27 };
+			var v = new double[] { 505, 352, 458, 220, 354, 414, 498, 545, 473, 543 };
+			var solver = new ZeroOneKnapsackSolver(w, v, 67);
+			var res = solver.GetSelectedItems(10, 67);
+			//Assert.AreEqual(res, 1270);
 		}
 	}
 }
